d75192b827517e498ff56f228988727c80755fa9
Jérémie Broutier <jeremie.broutier@gmail.com>
2019-04-11 11:13:15 +0200

    Added tests.

--- tests/Flight/FlightTest.php
+++ tests/Flight/FlightTest.php
@@ -14,18 +14,61 @@ final class FlightTest extends TestCase
 
     public function testGetArrivalAirport(): void
     {
+        $flight = new Flight(['estArrivalAirport' => 'EDDF']);
+
+        $this->assertIsString($flight->getArrivalAirport());
+        $this->assertEquals('EDDF', $flight->getArrivalAirport());
+
+        $flight = new Flight(['estArrivalAirport' => null]);
+
+        $this->assertNull($flight->getArrivalAirport());
     }
 
     public function testGetArrivalAirportCandidatesCount(): void
     {
+        $flight = new Flight(['arrivalAirportCandidatesCount' => 2]);
+
+        $this->assertIsInt($flight->getArrivalAirportCandidatesCount());
+        $this->assertEquals(2, $flight->getArrivalAirportCandidatesCount());
+
+        $flight = new Flight(['arrivalAirportCandidatesCount' => '2']);
+
+        $this->assertIsInt($flight->getArrivalAirportCandidatesCount());
+        $this->assertEquals(2, $flight->getArrivalAirportCandidatesCount());
     }
 
     public function testGetArrivalAirportHorizontalDistance(): void
     {
+        $flight = new Flight(['estArrivalAirportHorizDistance' => 1593]);
+
+        $this->assertIsInt($flight->getArrivalAirportHorizontalDistance());
+        $this->assertEquals(1593, $flight->getArrivalAirportHorizontalDistance());
+
+        $flight = new Flight(['estArrivalAirportHorizDistance' => '1593']);
+
+        $this->assertIsInt($flight->getArrivalAirportHorizontalDistance());
+        $this->assertEquals(1593, $flight->getArrivalAirportHorizontalDistance());
+
+        $flight = new Flight(['estArrivalAirportHorizDistance' => null]);
+
+        $this->assertNull($flight->getArrivalAirportHorizontalDistance());
     }
 
     public function testGetArrivalAirportVerticalDistance(): void
     {
+        $flight = new Flight(['estArrivalAirportVertDistance' => 95]);
+
+        $this->assertIsInt($flight->getArrivalAirportVerticalDistance());
+        $this->assertEquals(95, $flight->getArrivalAirportVerticalDistance());
+
+        $flight = new Flight(['estArrivalAirportVertDistance' => '95']);
+
+        $this->assertIsInt($flight->getArrivalAirportVerticalDistance());
+        $this->assertEquals(95, $flight->getArrivalAirportVerticalDistance());
+
+        $flight = new Flight(['estArrivalAirportVertDistance' => null]);
+
+        $this->assertNull($flight->getArrivalAirportVerticalDistance());
     }
 
     public function testGetArrivalTime(): void
